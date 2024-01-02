c55721ad2a9dfddc712431bf71003895dae683ee
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-01-02 13:25:17 +0530

    01.01.2024

--- 455. Assign Cookies.cpp
+++ 455. Assign Cookies.cpp
@@ -3,7 +3,6 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         ranges::sort(g);
         ranges::sort(s);
-
         int i = 0;
         for(int j = 0; j < s.size() && i < g.size(); ++j) {
             if(g[i] <= s[j]) {
