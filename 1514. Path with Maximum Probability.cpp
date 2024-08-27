29080d433c2534ef086a23ec87f70cfa949ea9dc
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-08-27 22:10:02 +0530

    27.08.2024

--- 1514. Path with Maximum Probability.cpp
+++ 1514. Path with Maximum Probability.cpp
@@ -1,9 +1,9 @@
 class Solution {
  public:
   double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-    // {a: [(b, probability_ab)]}
+    //{a: [(b, probability_ab)]}
     vector<vector<pair<int, double>>> graph(n);
-    // (the probability to reach u, u)
+    //(the probability to reach u, u)
     priority_queue<pair<double, int>> maxHeap;
     maxHeap.emplace(1.0, start);
     vector<bool> seen(n);
